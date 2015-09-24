d3aeab84f1f44b57f4bb76f4e515266c47b923df
Johnny Chan <jc2johnny@gmail.com>
2015-09-24 02:06:35 -0400

    refactored some code, in repl.it, it stops shorts sometimes arbitrarily when running code.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -42,11 +42,11 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    if (playerMove === "rock"){
-      if (computerMove === "rock"){
+    if (playerMove == "rock"){
+      if (computerMove == "rock"){
       winner = "tie";
       }
-      else if (computerMove === "paper") {
+      else if (computerMove == "paper") {
       winner = "computer";
       }
       else {
@@ -54,11 +54,11 @@ function getWinner(playerMove,computerMove) {
       }
     }
 
-    else if(playerMove === "paper"){
-      if (computerMove === "paper"){
+    else if(playerMove == "paper"){
+      if (computerMove == "paper"){
         winner = "tie";
       }
-      else if (computerMove === "scissors"){
+      else if (computerMove == "scissors"){
         winner = "computer";
       }
       else {
@@ -67,10 +67,10 @@ function getWinner(playerMove,computerMove) {
     }
 
     else {
-      if (computerMove === "scissors"){
+      if (computerMove == "scissors"){
         winner = "tie";
       }
-      else if (computerMove === "rock"){
+      else if (computerMove == "rock"){
         winner = "computer";
       }
       else {
@@ -80,9 +80,7 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
-var winner = "";
-var playerMove = "";
-var computerMove = "";
+
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
@@ -90,21 +88,21 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     while ((playerWins < 5) || computerWins < 5 ){
-       playerMove = getPlayerMove();
-       computerMove = getComputerMove();
-       winner = getWinner(playerMove, computerMove);
+      var  playerMove = getPlayerMove();
+      var computerMove = getComputerMove();
+      var winner = getWinner(playerMove, computerMove);
 
-      if (winner === "player") {
+      if (winner == "player") {
         console.log("Player chose " + playerMove + " and computer chose " + computerMove + " Player wins this round.");
         playerWins += 1;
       }
 
-      else if (winner === "computer"){
+      else if (winner == "computer"){
         console.log("Player chose " + playerMove + " and computer chose " + computerMove + " Computer wins this round.");
         computerWins += 1;
       }
 
-      else (winner === "tie"); {
+      else (winner == "tie"); {
         console.log("This round was a tie.");
       }
 
